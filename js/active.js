1fbe1d03ae76eb74c7b4c1871d01f76d030f7e87
TheHoltz <38303017+TheHoltz@users.noreply.github.com>
2019-09-06 08:10:09 -0300

    Add files via upload

---
+++ js/active.js
@@ -0,0 +1,10 @@
+
+//======>  Mobile Menu Activation
+
+$('.main-navigation').meanmenu({
+    meanScreenWidth: "992",
+    meanMenuContainer: '.mobile-menu',
+    meanMenuClose: "<i class='icon icon-simple-remove'></i>",
+    meanMenuOpen: "<i class='icon icon-menu-34'></i>",
+    meanExpand: "",
+});
